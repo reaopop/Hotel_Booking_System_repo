98a989486765b4d8b73969c1fbd53ae83d260784
reaopop <58912886+reaopop@users.noreply.github.com>
2022-11-08 13:59:38 +0200

    Mohamed 's Update

---
+++ Hotel_Booking_System/Controllers/BookingLogController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hotel_Booking_System.Controllers
+{
+    public class BookingLogController : Controller
+    {
+        // GET: BookingLog
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
